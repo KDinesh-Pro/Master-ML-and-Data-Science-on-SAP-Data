00faa649074e0b5df4cdd922b99ef75c8728d0ea
k.dinesh-pro <k.dinesh.indian@gmail.com>
2026-01-06 17:27:59 +0400

    Added Sections 3.1

--- SQLForProfessionals/SQLForProfesssionals/SQLFORPRO.sql
+++ SQLForProfessionals/SQLForProfesssionals/SQLFORPRO.sql
@@ -2,3 +2,9 @@
 --Create DATABASE SQLFORPro;
 
 Select name from sys.databases;
+--Section 3.1 Decimal & Numeric
+Select cast(123 AS decimal(5,2)) As 'Score'
+Select cast(123.980 AS numeric(10,5)) AS 'Team Score'
+--Section 3.2 Float & Real
+Select cast(PI() AS Float) AS 'PIE VAlue'
+Select cast (PI() AS Real) AS 'PIE Value in Short'
